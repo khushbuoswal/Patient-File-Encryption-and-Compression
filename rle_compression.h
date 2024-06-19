5788686c217d80588ea958bc444b6d26a6be2dd7
khushbu oswal <110380226+khushbuoswal@users.noreply.github.com>
2024-06-19 20:15:09 +1000

    Create rle_compression.h

---
+++ rle_compression.h
@@ -0,0 +1,22 @@
+#ifndef RLE_COMPRESSION_H
+#define RLE_COMPRESSION_H
+
+/*******************************************************************************
+ * This function compresses data from patients.txt
+ * inputs:
+ * - none
+ * outputs:
+ * - none
+*******************************************************************************/
+int compress();
+
+/*******************************************************************************
+ * This function decompresses data from patients.txt
+ * inputs:
+ * - none
+ * outputs:
+ * - none
+*******************************************************************************/
+int decompress();
+
+#endif /* RLE_COMPRESSION_H */
